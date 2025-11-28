3bd7e938994e1820cbfd8626f5c4b4ff89d46fbc
Vijay Mahadevan <vijay.m@gmail.com>
2025-11-27 18:11:38 -0800

    Improve the I/O for the source same as target workflow. Finally, usgs-rawdata.nc is working cleanly on Perlmutter with smoothing

--- src/MeshIO.cpp
+++ src/MeshIO.cpp
@@ -140,24 +140,85 @@ ErrorCode fetch_tag_as_float( Interface* mb,
 template<typename T>
 static ErrorCode write_variable(const PnetCDF::NcmpiVar& var, const ScalarRemapper::MeshData& mesh_data ) {
 
+    // 2D variable - handle USGS format with spatial chunking
+    std::vector<PnetCDF::NcmpiDim> dims = var.getDims();
+    size_t total_size = 1;
+    for (const auto& dim : dims) {
+        total_size *= dim.getSize();
+        LOG(INFO) << "Dimension " << dim.getName() << " has size " << dim.getSize();
+    }
+
     // Check if it's a double variable
     auto var_it = mesh_data.d_scalar_fields.find(var.getName());
     if (var_it != mesh_data.d_scalar_fields.end()) {
         const auto& values = var_it->second;
 
-        std::vector< T > castValues(values.size());
-        std::transform(values.begin(), values.end(), castValues.begin(), [](double d) { return static_cast<T>(d); });
-        // Vectorized weighted sum computation
-        var.putVar_all( castValues.data() );
+        if (dims.size() == 2)
+        {
+            size_t dim0_size = dims[0].getSize();
+            size_t dim1_size = dims[1].getSize();
+            const size_t MAX_ELEMENTS_PER_CHUNK = 250*1000*1000; // 100M elements, well below INT_MAX
+            size_t current_start = 0;
+            size_t lat_chunk_size = MAX_ELEMENTS_PER_CHUNK / dim1_size;
+            std::vector<MPI_Offset> start, read_count;
+            for (size_t lat_offset = 0; lat_offset < dim0_size; lat_offset += lat_chunk_size) {
+                size_t current_lat_count = std::min(lat_chunk_size, dim0_size - lat_offset);
+                size_t chunk_elements = current_lat_count * dim1_size;
+                LOG(INFO) << "\tWriting latitude chunk from " << lat_offset << " to " << lat_offset + current_lat_count << "." ;
+
+                std::vector<T> chunk_buffer(chunk_elements);
+                std::transform(values.begin() + current_start, values.begin() + current_start + chunk_elements,
+                                chunk_buffer.begin(), [](double d) { return static_cast<T>(d); });
+                start = {static_cast<MPI_Offset>(lat_offset), static_cast<MPI_Offset>(0)};
+                read_count = {static_cast<MPI_Offset>(current_lat_count), static_cast<MPI_Offset>(dims[1].getSize())};
+                var.putVar(start, read_count, chunk_buffer.data());
+                current_start += chunk_elements;
+            }
+            assert(current_start == total_size);
+        }
+        else {
+            std::vector< T > castValues(values.size());
+            std::transform(values.begin(), values.end(), castValues.begin(), [](double d) { return static_cast<T>(d); });
+            var.putVar( castValues.data() );
+        }
     } else {
         // Check if it's an integer variable
         auto ivar_it = mesh_data.i_scalar_fields.find(var.getName());
         if (ivar_it != mesh_data.i_scalar_fields.end()) {
             const auto& values = ivar_it->second;
 
-            std::vector< T > castValues(values.size());
-            std::transform(values.begin(), values.end(), castValues.begin(), [](int i) { return static_cast<T>(i); });
-            var.putVar_all( castValues.data() );
+            if (dims.size() == 2) {
+                size_t dim0_size = dims[0].getSize();
+                size_t dim1_size = dims[1].getSize();
+                const size_t MAX_ELEMENTS_PER_CHUNK = 250*1000*1000; // 100M elements, well below INT_MAX
+                size_t current_start = 0;
+                size_t lat_chunk_size = MAX_ELEMENTS_PER_CHUNK / dim1_size;
+                std::vector<MPI_Offset> start, read_count;
+                for (size_t lat_offset = 0; lat_offset < dim0_size; lat_offset += lat_chunk_size) {
+                    size_t current_lat_count = std::min(lat_chunk_size, dim0_size - lat_offset);
+                    size_t chunk_elements = current_lat_count * dim1_size;
+                    LOG(INFO) << "\tWriting latitude chunk from " << lat_offset << " to " << lat_offset + current_lat_count << "." ;
+
+                    start = {static_cast<MPI_Offset>(lat_offset), static_cast<MPI_Offset>(0)};
+                    read_count = {static_cast<MPI_Offset>(current_lat_count), static_cast<MPI_Offset>(dims[1].getSize())};
+                    if constexpr (std::is_same_v<T, int>) {
+                      var.putVar(start, read_count, values.data() + current_start);
+                    }
+                    else {
+                      std::vector<T> chunk_buffer(chunk_elements);
+                      std::transform(values.begin() + current_start, values.begin() + current_start + chunk_elements,
+                                      chunk_buffer.begin(), [](int i) { return static_cast<T>(i); });
+                      var.putVar(start, read_count, chunk_buffer.data());
+                    }
+                    current_start += chunk_elements;
+                }
+                assert(current_start == total_size);
+            }
+            else {
+                std::vector< T > castValues(values.size());
+                std::transform(values.begin(), values.end(), castValues.begin(), [](int i) { return static_cast<T>(i); });
+                var.putVar( castValues.data() );
+            }
         }
     }
 
@@ -176,60 +237,151 @@ ErrorCode NetcdfMeshIO::write_point_scalars_to_file( Interface* mb,
 
     const std::string dim_name = resolve_or_default( request.dimension_name, "ncol" );
 
-    // try
+    try
     {
         std::filesystem::copy_file( template_file,
                                     output_file,
                                     std::filesystem::copy_options::overwrite_existing );
 
-        PnetCDF::NcmpiFile out( MPI_COMM_WORLD, output_file.c_str(), PnetCDF::NcmpiFile::write, PnetCDF::NcmpiFile::classic5 );
-
-        auto vars = out.getVars();
-        for( const auto& name : request.scalar_var_names )
+// #pragma omp single
         {
-            for (const auto& ncvar : vars) {
-                if (ncvar.first == name) {
-                    const auto& variable = ncvar.second;
-                    if (variable.getType() == PnetCDF::ncmpiFloat) {
-                        MB_CHK_ERR( write_variable<float>( variable, mesh_data ) );
-                    } else if (variable.getType() == PnetCDF::ncmpiDouble) {
-                        MB_CHK_ERR( write_variable<double>( variable, mesh_data ) );
-                    } else if (variable.getType() == PnetCDF::ncmpiInt) {
-                        MB_CHK_ERR( write_variable<int>( variable, mesh_data ) );
-                    } else if (variable.getType() == PnetCDF::ncmpiShort) {
-                        MB_CHK_ERR( write_variable<short>( variable, mesh_data ) );
-                    } else if (variable.getType() == PnetCDF::ncmpiByte) {
-                        MB_CHK_ERR( write_variable<signed char>( variable, mesh_data ) );
-                    } else {
-                        MB_SET_ERR( MB_FAILURE, "Unsupported variable type for NetCDF write" );
-                    }
+            PnetCDF::NcmpiFile out( MPI_COMM_WORLD, output_file.c_str(), PnetCDF::NcmpiFile::write, PnetCDF::NcmpiFile::classic5 );
 
-                    if( request.verbose )
-                    {
-                        LOG( INFO ) << "Wrote NetCDF variable " << ncvar.first << " to " << output_file;
+            // Begin the independent data mode
+            int status = ncmpi_begin_indep_data(out.getId());
+            if (status != NC_NOERR) {
+                throw std::runtime_error("Failed to begin independent data mode for NetCDF write");
+            }
+
+            auto vars = out.getVars();
+            for( const auto& name : request.scalar_var_names )
+            {
+                for (const auto& ncvar : vars) {
+                    if (ncvar.first == name) {
+                        const auto& variable = ncvar.second;
+                        if (variable.getType() == PnetCDF::ncmpiFloat) {
+                            MB_CHK_ERR( write_variable<float>( variable, mesh_data ) );
+                        } else if (variable.getType() == PnetCDF::ncmpiDouble) {
+                            MB_CHK_ERR( write_variable<double>( variable, mesh_data ) );
+                        } else if (variable.getType() == PnetCDF::ncmpiInt) {
+                            MB_CHK_ERR( write_variable<int>( variable, mesh_data ) );
+                        } else if (variable.getType() == PnetCDF::ncmpiShort) {
+                            MB_CHK_ERR( write_variable<short>( variable, mesh_data ) );
+                        } else if (variable.getType() == PnetCDF::ncmpiByte) {
+                            MB_CHK_ERR( write_variable<signed char>( variable, mesh_data ) );
+                        } else {
+                            throw std::logic_error("Unsupported variable type for NetCDF write");
+                        }
+
+                        if( request.verbose )
+                        {
+                            LOG( INFO ) << "Wrote NetCDF variable " << ncvar.first << " to " << output_file;
+                        }
                     }
                 }
             }
+
+            // End independent data mode if you are done with independent operations
+            status = ncmpi_end_indep_data(out.getId());
+            if (status != NC_NOERR) {
+                throw std::runtime_error("Failed to end independent data mode for NetCDF write");
+            }
+
+            // for( const auto& base_name : request.squared_var_names )
+            // {
+            //     const std::string var_name = base_name + "_squared";
+            //     MB_CHK_ERR( write_variable( var_name ) );
+            // }
+
+            ncmpi_close( out.getId() );
         }
-
-        // for( const auto& base_name : request.squared_var_names )
-        // {
-        //     const std::string var_name = base_name + "_squared";
-        //     MB_CHK_ERR( write_variable( var_name ) );
-        // }
-
-        ncmpi_close( out.getId() );
     }
-    // catch( const std::filesystem::filesystem_error& e )
-    // {
-    //     MB_SET_ERR( MB_FAILURE, "File copy error for NetCDF output: " << e.what() );
-    // }
-    // catch( const PnetCDF::exceptions::NcmpiException& e )
-    // {
-    //     MB_SET_ERR( MB_FAILURE, "NetCDF error while writing " << output_file << ": " << e.what() );
-    // }
+    catch( const std::filesystem::filesystem_error& e )
+    {
+        MB_SET_ERR( MB_FAILURE, "File copy error for NetCDF output: " << e.what() );
+    }
+    catch( const PnetCDF::exceptions::NcmpiException& e )
+    {
+        MB_SET_ERR( MB_FAILURE, "NetCDF error while writing " << output_file << ": " << e.what() );
+    }
+    catch (const std::exception& e) {
+        MB_SET_ERR( MB_FAILURE, "Exception while writing " << output_file << ": " << e.what() );
+    }
 
     return MB_SUCCESS;
+
+// // #pragma omp single
+//     {
+//         try
+//         {
+//             std::filesystem::copy_file( template_file,
+//                                         output_file,
+//                                         std::filesystem::copy_options::overwrite_existing );
+
+//             PnetCDF::NcmpiFile out( MPI_COMM_WORLD, output_file.c_str(), PnetCDF::NcmpiFile::write, PnetCDF::NcmpiFile::classic5 );
+
+//             // Begin the independent data mode
+//             int status = ncmpi_begin_indep_data(out.getId());
+//             if (status != NC_NOERR) {
+//                 throw std::runtime_error("Failed to begin independent data mode for NetCDF write");
+//             }
+
+//             auto vars = out.getVars();
+//             for( const auto& name : request.scalar_var_names )
+//             {
+//                 for (const auto& ncvar : vars) {
+//                     if (ncvar.first == name) {
+//                         const auto& variable = ncvar.second;
+//                         if (variable.getType() == PnetCDF::ncmpiFloat) {
+//                             MB_CHK_ERR( write_variable<float>( variable, mesh_data ) );
+//                         } else if (variable.getType() == PnetCDF::ncmpiDouble) {
+//                             MB_CHK_ERR( write_variable<double>( variable, mesh_data ) );
+//                         } else if (variable.getType() == PnetCDF::ncmpiInt) {
+//                             MB_CHK_ERR( write_variable<int>( variable, mesh_data ) );
+//                         } else if (variable.getType() == PnetCDF::ncmpiShort) {
+//                             MB_CHK_ERR( write_variable<short>( variable, mesh_data ) );
+//                         } else if (variable.getType() == PnetCDF::ncmpiByte) {
+//                             MB_CHK_ERR( write_variable<signed char>( variable, mesh_data ) );
+//                         } else {
+//                             throw std::logic_error("Unsupported variable type for NetCDF write");
+//                         }
+
+//                         if( request.verbose )
+//                         {
+//                             LOG( INFO ) << "Wrote NetCDF variable " << ncvar.first << " to " << output_file;
+//                         }
+//                     }
+//                 }
+//             }
+
+//             // End independent data mode if you are done with independent operations
+//             status = ncmpi_end_indep_data(out.getId());
+//             if (status != NC_NOERR) {
+//                 throw std::runtime_error("Failed to end independent data mode for NetCDF write");
+//             }
+
+//             // for( const auto& base_name : request.squared_var_names )
+//             // {
+//             //     const std::string var_name = base_name + "_squared";
+//             //     MB_CHK_ERR( write_variable( var_name ) );
+//             // }
+
+//             ncmpi_close( out.getId() );
+//         }
+//         catch( const std::filesystem::filesystem_error& e )
+//         {
+//             MB_SET_ERR( MB_FAILURE, "File copy error for NetCDF output: " << e.what() );
+//         }
+//         catch( const PnetCDF::exceptions::NcmpiException& e )
+//         {
+//             MB_SET_ERR( MB_FAILURE, "NetCDF error while writing " << output_file << ": " << e.what() );
+//         }
+//         catch (const std::exception& e) {
+//             MB_SET_ERR( MB_FAILURE, "Exception while writing " << output_file << ": " << e.what() );
+//         }
+//     }
+
+//     return MB_SUCCESS;
 }
 
 ErrorCode NetcdfMeshIO::write_point_scalars_to_file( Interface* mb,
